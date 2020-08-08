e1042a8d55107bd621e5adbcb1886211dd041473
Raghu Betina <raghu@firstdraft.com>
2020-08-08 02:50:48 +0000

    Whitelist columns in DirectAssociation for ActiveAdmin

--- app/admin/direct_associations.rb
+++ app/admin/direct_associations.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register DirectAssociation do
 
+ permit_params :foreign_key, :name, :origin_model_id, :terminus_model_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
