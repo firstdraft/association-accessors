fb1012fbb17042b76b84eb81665517e360d48fc9
Ben Purinton <ben.purinton@gmail.com>
2025-06-23 15:08:27 +0200

    Revert "Fix SSO issue (#11)"
    This reverts commit fac2b107a659d28b2cc760837c13b5e24606a5cc.

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,18 +51,18 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[github]
 
   def self.from_omniauth(auth)
-    user = find_by(provider: auth.provider, uid: auth.uid)
+    user = find_by(email: auth.info.email)
     if user
-      user.email = auth.info.email.downcase if user.email != auth.info.email.downcase
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.github_access_token = auth.credentials.token
-      user.save
+      user.update(
+        provider: auth.provider,
+        uid: auth.uid,
+        github_access_token: auth.credentials.token
+      )
     else
       user = create do |new_user|
         new_user.provider = auth.provider
         new_user.uid = auth.uid
-        new_user.email = auth.info.email.downcase
+        new_user.email = auth.info.email
         new_user.password = Devise.friendly_token[0, 20]
         new_user.github_access_token = auth.credentials.token
       end
